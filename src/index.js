93f78a406bcfbbdd7e5b3e33bd57c5473302113f
Tristen <protonvert@gmail.com>
2023-03-12 16:04:02 -0500

    update for branch transfer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import Projects from './projects.js'
-import Task from './tasks.js'
 import Display from './displayController.js'
+import task from './tasks.js'
 
 const displayController = Display()
 const inbox = Projects('Inbox')
 
+inbox.addToTaskList(task('whatever', 'who cares', 'incomplete', new Date(), 'low'))
 
 
 displayController.addToSideBar(inbox)
+displayController.setProjectTitle(inbox.getProjectName())
+displayController.loadTasks(inbox)
+displayController.getProjectItems()
+
+// for local storage to work, every project needs to be saved to a projectList whenever created
+
 
 // ----------------------------------------------------
